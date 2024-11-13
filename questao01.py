345d5acea9a78b4923dfb694f4c7c76b796979d9
erikitafaria <erikitafaria@yahoo.com.br>
2024-11-13 11:28:19 -0300

    questao01.py

--- questao01.py
+++ questao01.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.write('sou servidora pública')
 #não vou ver a mensagem aqui por qu
+st.title("Este é o título do app")
+st.header("Este é o subtítulo")
+st.subheader("Este é o terceiro subtítulo")
